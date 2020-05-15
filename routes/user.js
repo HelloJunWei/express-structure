3bf7a4e8283da0c5f1b58529709ef6944598a62c
Neil Guo <neilguo@aimatchmaker.com>
2020-05-15 21:27:41 +0800

    user restful API 修改

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,17 @@
-const api = require('../utils/Api')('user')
+const api = require('../utils/api')('user')
 const User = require('../models/user')
 const { query, validationResult } = require('express-validator')
 
 api.get('/user', [
   query('user_id').notEmpty().isString().trim().escape().custom(value => {
-    return [1, 2].indexOf(value) !== -1}).withMessage('error user_id'),
+    return [1, 2].indexOf(parseInt(value)) !== -1}).withMessage('error user_id'),
 ], (req, res) => {
   // 利用 express-validator 做參數的驗證~
   // 如果不符合設定規則就回傳 error status code
   const errors = validationResult(req)
   if (!errors.isEmpty()) throw [400, errors.array()[0].msg]
   const user_id = req.query.user_id
-  const user = User.user_list.find(obj => obj.id === req.query.id)
+    const user = User.user_list.find(obj => obj.id === parseInt(user_id))
   res.ok({ user })
 
 })
