1d49719c259a1574d0320c1e127d5d617f7b7467
Neil Guo <neilguo@aimatchmaker.com>
2020-05-15 21:00:32 +0800

    models 放入範例資料

---
+++ models/product.js
@@ -0,0 +1,14 @@
+module.exports = {
+  product_list: [
+    {
+      id: 1,
+      name: 'iphone',
+      create_time: new Date('01 Feb 2010 00: 00: 00 GMT')
+    },
+    {
+      id: 2,
+      name: 'ipad',
+      create_time: new Date('03 Feb 2010 00: 00: 00 GMT')
+    }
+  ]
+}
---
+++ models/user.js
@@ -0,0 +1,14 @@
+module.exports = {
+  user_list:[
+    {
+      id: 1,
+      name: 'Neil',
+      create_time: new Date('01 Feb 2020 00: 00: 00 GMT')
+    },
+    {
+      id: 2,
+      name: 'John',
+      create_time: new Date('03 Feb 2020 00: 00: 00 GMT')
+    }
+  ]
+}
