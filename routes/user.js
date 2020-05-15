e16e683a99c2e50d51379ff66e6c2350f195e89b
Neil Guo <neilguo@aimatchmaker.com>
2020-05-15 21:06:55 +0800

    route/user.js 新增 restful API

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const api = require('../utils/Api')('user')
+const User = require('../models/user')
+const { query, validationResult } = require('express-validator')
+
+api.get('/user', [
+  query('user_id').notEmpty().isString().trim().escape().custom(value => {
+    return [1, 2].indexOf(value) !== -1}).withMessage('error user_id'),
+], (req, res) => {
+  // 利用 express-validator 做參數的驗證~
+  // 如果不符合設定規則就回傳 error status code
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) throw [400, errors.array()[0].msg]
+  const user_id = req.query.user_id
+  const user = User.user_list.find(obj => obj.id === req.query.id)
+  res.ok({ user })
+
+})
+
+module.exports = api
